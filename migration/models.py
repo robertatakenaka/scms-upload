aa95a12068a6f360b8e9edecaa2120286189996f
Roberta Takenaka <roberta.takenaka@scielo.org>
2022-08-19 15:37:36 -0300

    Cria os modelos para registrar migração e documento migrado

--- migration/models.py
+++ migration/models.py
@@ -58,7 +58,7 @@ class IssueMigrationTracker(CommonControlField):
     acron = models.CharField(
         _('Acronym'), max_length=20, null=False, blank=False)
     issue_pid = models.CharField(
-        _('Issue PID'), max_length=9, null=False, blank=False)
+        _('Issue PID'), max_length=17, null=False, blank=False)
     year = models.CharField(
         _('Publication Year'), max_length=4, null=False, blank=False
     )
@@ -88,7 +88,50 @@ class MigratedIssue(CommonControlField):
     issue_pid = models.CharField(
         _('Issue PID'), max_length=9, null=False, blank=False)
     # registro no formato json correspondente ao conteúdo da base isis
-    record = models.JSONField(blank=False)
+    records = models.JSONField(blank=False)
 
     def __str__(self):
         return f"{self.issue_pid}"
+
+
+class DocumentMigrationTracker(CommonControlField):
+
+    scielo_issn = models.CharField(
+        _('SciELO ISSN'), max_length=9, null=False, blank=False)
+    acron = models.CharField(
+        _('Acronym'), max_length=20, null=False, blank=False)
+    pid = models.CharField(
+        _('Document PID'), max_length=9, null=False, blank=False)
+    year = models.CharField(
+        _('Publication Year'), max_length=4, null=False, blank=False
+    )
+
+    # datas no registro da base isis para identificar
+    # se houve mudança durante a migração
+    isis_updated_date = models.CharField(
+        _('ISIS updated date'), max_length=8, null=False, blank=False)
+    isis_created_date = models.CharField(
+        _('ISIS created date'), max_length=8, null=False, blank=False)
+
+    # status do registro quanto aos metadados
+    status = models.CharField(
+        _('Status'), max_length=20,
+        choices=choices.MIGRATION_STATUS,
+        default=choices.MS_TO_MIGRATE,
+    )
+
+    migrated_doc = models.ForeignKey('MigratedDocument')
+
+    def __str__(self):
+        return f"{self.acron} {self.pid} {self.status}"
+
+
+class MigratedDocument(CommonControlField):
+
+    pid = models.CharField(
+        _('Document PID'), max_length=23, null=False, blank=False)
+    # registro no formato json correspondente ao conteúdo da base isis
+    record = models.JSONField(blank=False)
+
+    def __str__(self):
+        return f"{self.pid}"
