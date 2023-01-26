cad902f0fb96213d8703d8ddd2afb4403d0d2069
Roberta Takenaka <roberta.takenaka@scielo.org>
2023-01-26 17:36:09 -0300

    Move algumas funções de controller para classmethod de models

--- journal/choices.py
+++ journal/choices.py
@@ -6,3 +6,28 @@ SOCIAL_NETWORK_NAMES = [
     ('twitter', 'Twitter'),
     ('journal', _('Journal URL')),
 ]
+
+
+# JOURNAL_PUBLICATION_STATUS
+# Embora alguns periódicos estão encerrados (CEASED) ou suspensos (SUSPENDED),
+# eles ainda ficam disponíveis no site (`JOURNAL_AT_WEBSITE_STATUS = CURRENT`)
+CURRENT = 'C'
+NOT_INFORMED = ''
+CEASED = 'D'
+UNKNOWN = '?'
+SUSPENDED = 'S'
+
+JOURNAL_PUBLICATION_STATUS = [
+    (SUSPENDED, _('Suspended')),
+    (UNKNOWN, _('Unknown')),
+    (CEASED, _('Ceased')),
+    (NOT_INFORMED, _('Not informed')),
+    (CURRENT, _('Current')),
+]
+
+
+# AVAILABILTY on the website
+JOURNAL_AVAILABILTY_STATUS = [
+    (UNKNOWN, _('Unknown')),
+    (CURRENT, _('Current')),
+]
