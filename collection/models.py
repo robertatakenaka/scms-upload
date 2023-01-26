e45dac169c6979e375a17bf51f5c6144ecbe1601
Roberta Takenaka <roberta.takenaka@scielo.org>
2023-01-26 16:21:12 -0300

    Remove os modelos SciELOJournal, SciELOIssue, SciELODocument, cujos atributos serão unidos aos atributos dos modelos da app migration

--- collection/models.py
+++ collection/models.py
@@ -1,19 +1,41 @@
+import logging
+from datetime import datetime
+from copy import deepcopy
+
+import requests
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from packtools.sps.models.article_renditions import ArticleRenditions
+from packtools.sps.models.related_articles import RelatedItems
+from packtools.sps.models.article_assets import (
+    ArticleAssets,
+    SupplementaryMaterials,
+)
+from packtools.sps.models.article_ids import ArticleIds
 
-from core.models import CommonControlField
+from core.choices import LANGUAGE
+from core.models import CommonControlField, Language
 from core.forms import CoreAdminModelForm
-
+from xmlsps.xml_sps_lib import get_xml_with_pre_from_uri
 from journal.models import OfficialJournal
 from issue.models import Issue
 from article.models import Article
 from .choices import JOURNAL_AVAILABILTY_STATUS, WEBSITE_KIND
+from . import exceptions
+from files_storage.models import MinioFile
+from files_storage.utils import generate_finger_print
 
 
 class Collection(CommonControlField):
     """
     Class that represent the Collection
     """
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['name']),
+            models.Index(fields=['acron']),
+        ]
 
     def __unicode__(self):
         return u'%s %s' % (self.name, self.acron)
@@ -26,146 +48,89 @@ class Collection(CommonControlField):
 
     base_form_class = CoreAdminModelForm
 
-
-class SciELOJournal(CommonControlField):
-    """
-    Class that represents journals data in a SciELO Collection context
-    Its attributes are related to the journal in collection
-    For official data, use Journal model
-    """
-    collection = models.ForeignKey(Collection, on_delete=models.CASCADE)
-    scielo_issn = models.CharField(_('SciELO ISSN'), max_length=9, null=False, blank=False)
-    acron = models.CharField(_('Acronym'), max_length=25, null=True, blank=True)
-    title = models.CharField(_('Title'), max_length=255, null=True, blank=True)
-    availability_status = models.CharField(
-        _('Availability Status'), max_length=10, null=True, blank=True,
-        choices=JOURNAL_AVAILABILTY_STATUS)
-    official_journal = models.ForeignKey(
-        OfficialJournal, on_delete=models.SET_NULL, null=True)
-
-    class Meta:
-        unique_together = [
-            ['collection', 'scielo_issn'],
-            ['collection', 'acron'],
-        ]
-        indexes = [
-            models.Index(fields=['acron']),
-            models.Index(fields=['collection']),
-            models.Index(fields=['scielo_issn']),
-            models.Index(fields=['availability_status']),
-            models.Index(fields=['official_journal']),
-        ]
-
-    def __unicode__(self):
-        return u'%s %s' % (self.collection, self.scielo_issn)
-
-    def __str__(self):
-        return u'%s %s' % (self.collection, self.scielo_issn)
+    @classmethod
+    def get_or_create(cls, acron, creator, name=None):
+        try:
+            return cls.objects.get(acron=acron)
+        except cls.DoesNotExist:
+            collection = cls()
+            collection.acron = acron
+            collection.name = name
+            collection.creator = creator
+            collection.save()
+            return collection
+        except Exception as e:
+            raise exceptions.GetOrCreateCollectionError(
+                _('Unable to get_or_create_collection {} {} {}').format(
+                    acron, type(e), e
+                )
+            )
 
 
-class SciELOIssue(CommonControlField):
-    """
-    Class that represents an issue in a SciELO Collection
-    Its attributes are related to the issue in collection
-    For official data, use Issue model
-    """
-
-    def __unicode__(self):
-        return u'%s %s' % (self.scielo_journal, self.issue_pid)
-
-    def __str__(self):
-        return u'%s %s' % (self.scielo_journal, self.issue_pid)
-
-    scielo_journal = models.ForeignKey(
-        SciELOJournal, on_delete=models.SET_NULL, null=True)
-    issue_pid = models.CharField(_('Issue PID'), max_length=23, null=False, blank=False)
-    # v30n1 ou 2019nahead
-    issue_folder = models.CharField(_('Issue Folder'), max_length=23, null=False, blank=False)
-    official_issue = models.ForeignKey(
-        Issue, on_delete=models.SET_NULL, null=True)
-
-    class Meta:
-        unique_together = [
-            ['scielo_journal', 'issue_pid'],
-            ['scielo_journal', 'issue_folder'],
-            ['issue_pid', 'issue_folder'],
-        ]
-        indexes = [
-            models.Index(fields=['scielo_journal']),
-            models.Index(fields=['issue_pid']),
-            models.Index(fields=['issue_folder']),
-            models.Index(fields=['official_issue']),
-        ]
-
-
-class SciELODocument(CommonControlField):
-    """
-    Class that represents a document in a SciELO Collection
-    Its attributes are related to the document in collection
-    For official data, use Article model
-    """
-
-    def __unicode__(self):
-        return u'%s %s' % (self.scielo_issue, self.pid)
-
-    def __str__(self):
-        return u'%s %s' % (self.scielo_issue, self.pid)
-
-    scielo_issue = models.ForeignKey(SciELOIssue, on_delete=models.CASCADE)
-    pid = models.CharField(_('PID'), max_length=23, null=True, blank=True)
-    file_id = models.CharField(_('File ID'), max_length=50, null=True, blank=True)
-    official_document = models.ForeignKey(Article, on_delete=models.SET_NULL, null=True, blank=True)
-
-    xml_files = models.ManyToManyField('XMLFile', null=True, related_name='xml_files')
-    renditions_files = models.ManyToManyField('FileWithLang', null=True, related_name='renditions_files')
-    html_files = models.ManyToManyField('SciELOHTMLFile', null=True, related_name='html_files')
-
-    class Meta:
-        unique_together = [
-            ['scielo_issue', 'pid'],
-            ['scielo_issue', 'file_id'],
-            ['pid', 'file_id'],
-        ]
-        indexes = [
-            models.Index(fields=['scielo_issue']),
-            models.Index(fields=['pid']),
-            models.Index(fields=['file_id']),
-            models.Index(fields=['official_document']),
-        ]
-
-
-class SciELOFile(models.Model):
-    scielo_issue = models.ForeignKey(SciELOIssue, on_delete=models.CASCADE)
-    file_id = models.CharField(_('ID'), max_length=255, null=True, blank=True)
+class SciELOFile(CommonControlField):
+    pkg_name = models.CharField(_('Package name'), max_length=65, null=True, blank=True)
     relative_path = models.CharField(_('Relative Path'), max_length=255, null=True, blank=True)
-    name = models.CharField(_('Filename'), max_length=255, null=False, blank=False)
-    uri = models.URLField(_('URI'), max_length=255, null=True)
-    object_name = models.CharField(_('Object name'), max_length=255, null=True)
-
-    def __str__(self):
-        return f"{self.scielo_issue} {self.name}"
+    remote_file = models.ForeignKey(MinioFile, null=True, blank=True, on_delete=models.SET_NULL)
+    langs = models.ManyToManyField(Language)
 
     class Meta:
-
         indexes = [
-            models.Index(fields=['file_id']),
+            models.Index(fields=['pkg_name']),
             models.Index(fields=['relative_path']),
-            models.Index(fields=['name']),
-            models.Index(fields=['object_name']),
-            models.Index(fields=['scielo_issue']),
+            models.Index(fields=['remote_file']),
         ]
+
+    @property
+    def name(self):
+        return (
+            self.remote_file and self.remote_file.basename or
+            os.path.basename(self.relative_path))
+
+    @property
+    def uri(self):
+        if self.remote_file:
+            return self.remote_file.uri
+
+    def __str__(self):
+        return self.relative_path or str(self.remote_file)
+
+    @classmethod
+    def get_or_create(cls, item, creator):
+        try:
+            return cls.objects.get(relative_path=item['relative_path'])
+        except cls.DoesNotExist:
+            file = cls()
+            file.pkg_name = item['key']
+            file.relative_path = item.get('relative_path')
+            file.creator = creator
+            file.save()
+            return file
+
+    @classmethod
+    def create_or_update(cls, item, push_file, subdirs, preserve_name, creator):
+        obj = cls.get_or_create(item)
+
+        response = push_file(
+            obj,
+            item['path'],
+            subdirs,
+            preserve_name,
+            creator,
+        )
+        for k in item.keys():
+            if hasattr(obj, k):
+                setattr(obj, k, getattr(obj, k) or item[k])
+        return obj
 
 
 class FileWithLang(SciELOFile):
 
-    lang = models.CharField(
-        _('Language'), max_length=4, null=False, blank=False)
+    lang = models.ForeignKey(Language, null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self):
-        return f"{self.scielo_issue} {self.name} {self.lang}"
+        return f"{super()} {self.lang}"
 
     class Meta:
-
         indexes = [
             models.Index(fields=['lang']),
         ]
@@ -175,10 +140,9 @@ class AssetFile(SciELOFile):
     is_supplementary_material = models.BooleanField(default=False)
 
     def __str__(self):
-        return f"{self.scielo_issue} {self.name} {self.is_supplementary_material}"
+        return f"{super()} {self.is_supplementary_material}"
 
     class Meta:
-
         indexes = [
             models.Index(fields=['is_supplementary_material']),
         ]
@@ -186,28 +150,137 @@ class AssetFile(SciELOFile):
 
 class XMLFile(FileWithLang):
     assets_files = models.ManyToManyField('AssetFile')
-    languages = models.JSONField(null=True)
-
-    public_uri = models.URLField(_('Public URI'), max_length=255, null=True)
-    public_object_name = models.CharField(_('Public object name'), max_length=255, null=True)
 
     def __str__(self):
-        return f"{self.scielo_issue} {self.name} {self.lang} {self.languages}"
+        return f"{super()}"
 
+    def tostring(self):
+        return self.xml_with_pre.tostring()
 
-class SciELOHTMLFile(FileWithLang):
-    part = models.CharField(
-        _('Part'), max_length=6, null=False, blank=False)
-    assets_files = models.ManyToManyField('AssetFile')
+    @property
+    def xml_with_pre(self):
+        if not hasattr(self, '_xml_with_pre') or not self._xml_with_pre:
+            try:
+                self._xml_with_pre = get_xml_with_pre_from_uri(self.uri)
+            except Exception as e:
+                raise exceptions.XMLFileXMLWithPreError(
+                    _("Unable to get XML with pre (XMLFile) {}: {} {}").format(
+                        self.uri, type(e), e
+                    )
+                )
+        return self._xml_with_pre
 
-    def __str__(self):
-        return f"{self.scielo_issue} {self.name} {self.lang} {self.part}"
+    @property
+    def related_articles(self):
+        if not hasattr(self, '_related_articles') or not self._related_articles:
+            self._related_articles = self.xml_with_pre.related_items
+        return self._related_articles
 
-    class Meta:
+    @property
+    def supplementary_materials(self):
+        if not hasattr(self, '_supplementary_materials') or not self._supplementary_materials:
+            supplmats = SupplementaryMaterials(self.xml_with_pre.xmltree)
+            self._supplementary_materials = []
+            names = [item.name for item in suppl_mats.items]
+            for asset_file in self.assets_files:
+                if asset_file.name in names:
+                    asset_file.is_supplementary_material = True
+                    asset_file.save()
+                if asset_file.is_supplementary_material:
+                    self._supplementary_materials.append({
+                        "uri": asset_file.uri,
+                        "lang": self.lang,
+                        "ref_id": None,
+                        "filename": asset_file.name,
+                    })
+        return self._supplementary_materials
 
-        indexes = [
-            models.Index(fields=['part']),
+    def add_assets(self, issue_assets_dict):
+        """
+        Atribui asset_files
+        """
+        try:
+            # obtém os assets do XML
+            article_assets = ArticleAssets(self.xml_with_pre.xmltree)
+            for asset_in_xml in article_assets.article_assets:
+                asset = issue_assets_dict.get(asset_in_xml.name)
+                if asset:
+                    # FIXME tratar asset_file nao encontrado
+                    self.assets_files.add(asset)
+            self.save()
+        except Exception as e:
+            raise exceptions.AddAssetFilesError(
+                _("Unable to add assets to public XML to {} {} {})").format(
+                    xml_file, type(e), e
+                ))
+
+    def get_xml_with_pre_with_remote_assets(self, issue_assets_uris):
+        # FIXME assets de artigo pode estar em qq outra pasta do periódico
+        # há casos em que os assets do artigo VoR está na pasta ahead
+        xml_with_pre = deepcopy(self.xml_with_pre)
+        article_assets = ArticleAssets(xml_with_pre.xmltree)
+        article_assets.replace_names(issue_assets_uris)
+        return {"xml_with_pre": xml_with_pre, "name": self.pkg_name}
+
+    def set_langs(self):
+        try:
+            article = ArticleRenditions(self.xml_with_pre.xmltree)
+            renditions = article.article_renditions
+            self.lang = renditions[0].language
+            for rendition in renditions:
+                self.langs.add(
+                    Language.get_or_create(code2=rendition.language)
+                )
+            self.save()
+        except Exception as e:
+            raise exceptions.AddLangsToXMLFilesError(
+                _("Unable to set main lang to xml {}: {} {}").format(
+                    self.uri, type(e), e
+                )
+            )
+
+    @property
+    def languages(self):
+        return [
+            {"lang": lang.code2 for lang in self.langs.iterator()}
         ]
+
+    # FIXME
+    # @property
+    # def xml_files_with_lang(self):
+    #     if not hasattr(self, '_xml_files_with_lang') or not self._xml_files_with_lang:
+    #         self._xml_files_with_lang = {}
+    #         for xml_file in self.xml_files:
+    #             self._xml_files_with_lang[xml_file.lang] = xml_file
+    #     return self._xml_files_with_lang
+
+    # @property
+    # def text_langs(self):
+    #     if not hasattr(self, '_text_langs') or not self._text_langs:
+    #         self._text_langs = [
+    #             {"lang": lang}
+    #             for lang in self.xml_files_with_lang.keys()
+    #         ]
+    #     return self._text_langs
+
+    # @property
+    # def related_items(self):
+    #     if not hasattr(self, '_related_items') or not self._related_items:
+    #         items = []
+    #         for lang, xml_file in self.xml_files_with_lang.items():
+    #             items.extend(xml_file.related_articles)
+    #         self._related_items = items
+    #     return self._related_items
+
+    # @property
+    # def supplementary_materials(self):
+    #     if not hasattr(self, '_supplementary_materials') or not self._supplementary_materials:
+    #         items = []
+    #         for lang, xml_file in self.xml_files_with_lang.items():
+    #             items.extend(xml_file.supplementary_materials)
+    #         self._supplementary_materials = items
+    #     return self._supplementary_materials
+
 
 
 class NewWebSiteConfiguration(CommonControlField):
@@ -220,6 +293,18 @@ class NewWebSiteConfiguration(CommonControlField):
     def __str__(self):
         return f"{self.url}"
 
+    @classmethod
+    def get_or_create(cls, url, db_uri=None, creator=None):
+        try:
+            return cls.objects.get(url=url)
+        except cls.DoesNotExist:
+            new_website_config = cls()
+            new_website_config.db_uri = db_uri
+            new_website_config.url = url
+            new_website_config.creator = creator
+            new_website_config.save()
+            return new_website_config
+
     class Meta:
         indexes = [
             models.Index(fields=['url']),
@@ -228,46 +313,9 @@ class NewWebSiteConfiguration(CommonControlField):
     base_form_class = CoreAdminModelForm
 
 
-class FilesStorageConfiguration(CommonControlField):
-
-    host = models.CharField(
-        _('Host'), max_length=255, null=True, blank=True)
-    bucket_root = models.CharField(
-        _('Bucket root'), max_length=255, null=True, blank=True)
-    bucket_app_subdir = models.CharField(
-        _('Bucket app subdir'), max_length=64, null=True, blank=True)
-    bucket_public_subdir = models.CharField(
-        _('Bucket public subdir'), max_length=64, null=True, blank=True)
-    bucket_migration_subdir = models.CharField(
-        _('Bucket migration subdir'), max_length=64, null=True, blank=True)
-    bucket_temp_subdir = models.CharField(
-        _('Bucket temp subdir'), max_length=64, null=True, blank=True)
-    bucket_versions_subdir = models.CharField(
-        _('Bucket versions subdir'), max_length=64, null=True, blank=True)
-    access_key = models.CharField(
-        _('Access key'), max_length=255, null=True, blank=True)
-    secret_key = models.CharField(
-        _('Secret key'), max_length=255, null=True, blank=True)
-    secure = models.BooleanField(_('Secure'), default=True)
-
-    def __str__(self):
-        return f"{self.host} {self.bucket_root}"
-
-    class Meta:
-        unique_together = [
-            ['host', 'bucket_root'],
-        ]
-        indexes = [
-            models.Index(fields=['host']),
-            models.Index(fields=['bucket_root']),
-        ]
-
-    base_form_class = CoreAdminModelForm
-
-
 class ClassicWebsiteConfiguration(CommonControlField):
 
-    collection = models.ForeignKey(Collection, on_delete=models.CASCADE)
+    collection = models.ForeignKey(Collection, on_delete=models.SET_NULL, null=True, blank=True)
 
     title_path = models.CharField(
         _('Title path'), max_length=255, null=True, blank=True,
@@ -299,6 +347,32 @@ class ClassicWebsiteConfiguration(CommonControlField):
 
     def __str__(self):
         return f"{self.collection}"
+
+    @classmethod
+    def get_or_create(cls, collection, config, user):
+        try:
+            return cls.objects.get(collection=collection)
+        except cls.DoesNotExist:
+            classic_website = cls()
+            classic_website.collection = collection
+            classic_website.title_path = config['title_path']
+            classic_website.issue_path = config['issue_path']
+            classic_website.serial_path = config['SERIAL_PATH']
+            classic_website.cisis_path = config.get('CISIS_PATH')
+            classic_website.bases_work_path = config['BASES_WORK_PATH']
+            classic_website.bases_pdf_path = config['BASES_PDF_PATH']
+            classic_website.bases_translation_path = (
+                config['BASES_TRANSLATION_PATH']
+            )
+            classic_website.bases_xml_path = (
+                config['BASES_XML_PATH']
+            )
+            classic_website.htdocs_img_revistas_path = (
+                config['HTDOCS_IMG_REVISTAS_PATH']
+            )
+            classic_website.creator = user
+            classic_website.save()
+            return classic_website
 
     class Meta:
         indexes = [
