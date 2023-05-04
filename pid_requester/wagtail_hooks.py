96701fc8b2a1aff8983acc940ba39d2230c14047
Roberta Takenaka <roberta.takenaka@scielo.org>
2023-05-04 17:42:04 -0300

    Cria formulário para gerir a configuração do pid provider

--- pid_requester/wagtail_hooks.py
+++ pid_requester/wagtail_hooks.py
@@ -7,7 +7,8 @@ from wagtail.contrib.modeladmin.options import (
 )
 from wagtail.contrib.modeladmin.views import CreateView
 
-from .models import PidRequesterBadRequest
+from config.menu import get_menu_order
+from .models import PidRequesterBadRequest, PidProviderConfig
 
 
 class PidRequesterBadRequestCreateView(CreateView):
@@ -41,4 +42,39 @@ class PidRequesterBadRequestAdmin(ModelAdmin):
     )
 
 
-modeladmin_register(PidRequesterBadRequestAdmin)
+class PidProviderConfigCreateView(CreateView):
+    def form_valid(self, form):
+        self.object = form.save_all(self.request.user)
+        return HttpResponseRedirect(self.get_success_url())
+
+
+class PidProviderAdmin(ModelAdmin):
+    model = PidProviderConfig
+    inspect_view_enabled = True
+    menu_label = _("Pid Provider Configuration")
+    create_view_class = PidProviderConfigCreateView
+    menu_icon = "folder"
+    menu_order = 300
+    add_to_settings_menu = False
+    exclude_from_explorer = False
+
+    list_display = (
+        "pid_provider_api_post_xml",
+        "pid_provider_api_get_token",
+        "api_username",
+        "api_password",
+        "timeout",
+    )
+
+
+class PidProviderAdminGroup(ModelAdminGroup):
+    menu_label = _("Pid Provider")
+    menu_icon = "folder-open-inverse"
+    menu_order = get_menu_order("pid-provider")
+    items = (
+        PidProviderAdmin,
+        PidRequesterBadRequestAdmin,
+    )
+
+
+modeladmin_register(PidProviderAdminGroup)
