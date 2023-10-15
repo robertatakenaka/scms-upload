0b871d13fae2754b6d8af7bde2bb5474f35770b1
Roberta Takenaka <roberta.takenaka@scielo.org>
2023-10-15 00:35:44 -0300

    Faz modificações em migration/controller - Troca os parâmetros creator dos create_or_update por user - Modifica a forma de registrar as falhas de migração

--- migration/controller.py
+++ migration/controller.py
@@ -1,12 +1,13 @@
-import sys
 import logging
 import os
+import sys
 from datetime import datetime
 
 from django.utils.translation import gettext_lazy as _
 
 from collection.models import Collection
 from core.controller import parse_yyyymmdd
+from core.models import GlobalIncidentTracker
 from issue.models import Issue, SciELOIssue
 from journal.models import Journal, OfficialJournal, SciELOJournal
 from package import choices as package_choices
@@ -16,8 +17,8 @@ from scielo_classic_website import classic_ws
 from . import exceptions
 from .choices import (
     DOC_GENERATED_SPS_PKG,
-    DOC_TO_GENERATE_XML,
     DOC_TO_GENERATE_SPS_PKG,
+    DOC_TO_GENERATE_XML,
     MS_IMPORTED,
     MS_TO_IGNORE,
     MS_TO_MIGRATE,
@@ -25,28 +26,43 @@ from .choices import (
 from .models import (
     ClassicWebsiteConfiguration,
     MigratedDocument,
+    MigratedDocumentHTML,
     MigratedIssue,
     MigratedJournal,
-    MigrationFailure,
-    MigratedDocumentHTML,
 )
 
 
-def get_classic_website(collection_acron):
-    logging.info(f"collection_acron={collection_acron}")
-    config = ClassicWebsiteConfiguration.objects.get(collection__acron=collection_acron)
-    return classic_ws.ClassicWebsite(
-        bases_path=os.path.join(os.path.dirname(config.bases_work_path), "bases"),
-        bases_work_path=config.bases_work_path,
-        bases_translation_path=config.bases_translation_path,
-        bases_pdf_path=config.bases_pdf_path,
-        bases_xml_path=config.bases_xml_path,
-        htdocs_img_revistas_path=config.htdocs_img_revistas_path,
-        serial_path=config.serial_path,
-        cisis_path=config.cisis_path,
-        title_path=config.title_path,
-        issue_path=config.issue_path,
-    )
+def get_classic_website(collection_acron, user):
+    try:
+        logging.info(f"collection_acron={collection_acron}")
+        config = ClassicWebsiteConfiguration.objects.get(
+            collection__acron=collection_acron
+        )
+        return classic_ws.ClassicWebsite(
+            bases_path=os.path.join(os.path.dirname(config.bases_work_path), "bases"),
+            bases_work_path=config.bases_work_path,
+            bases_translation_path=config.bases_translation_path,
+            bases_pdf_path=config.bases_pdf_path,
+            bases_xml_path=config.bases_xml_path,
+            htdocs_img_revistas_path=config.htdocs_img_revistas_path,
+            serial_path=config.serial_path,
+            cisis_path=config.cisis_path,
+            title_path=config.title_path,
+            issue_path=config.issue_path,
+        )
+    except Exception as e:
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        GlobalIncidentTracker.register_incident(
+            collection_acron=collection_acron,
+            app_name="migration",
+            item_name="collection",
+            item_id=collection_acron,
+            message=None,
+            context="get-classic-website-configuration",
+            e=e,
+            exc_traceback=exc_traceback,
+            user=user,
+        )
 
 
 def migrate_title_db(
@@ -54,19 +70,91 @@ def migrate_title_db(
     collection,
     force_update=False,
 ):
-    classic_website = get_classic_website(collection.acron)
+    classic_website = get_classic_website(collection.acron, user)
 
-    for (
-        scielo_issn,
-        journal_data,
-    ) in classic_website.get_journals_pids_and_records():
-        migrated_journal = create_or_update_migrated_journal_record(
-            user,
-            collection,
+    try:
+        for (
             scielo_issn,
-            journal_data[0],
-            force_update,
+            journal_data,
+        ) in classic_website.get_journals_pids_and_records():
+            migrated_journal = create_or_update_migrated_journal_record(
+                user,
+                collection,
+                scielo_issn,
+                journal_data[0],
+                force_update,
+            )
+    except Exception as e:
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        GlobalIncidentTracker.register_incident(
+            collection_acron=collection.acron,
+            app_name="migration",
+            item_name="journals",
+            item_id=None,
+            message=None,
+            context="get_journals_pids_and_records",
+            e=e,
+            exc_traceback=exc_traceback,
+            user=user,
         )
+
+
+# def create_or_update_migrated_journal_record(
+#     user,
+#     collection,
+#     scielo_issn,
+#     journal_data,
+#     force_update=False,
+# ):
+#     try:
+#         migrated_journal = None
+
+#         context = "get-data-from-classic-website"
+#         classic_website_journal = classic_ws.Journal(journal_data)
+
+#         context = "migrate-data"
+#         migrated_journal = MigratedJournal.create_or_update(
+#             collection=collection,
+#             pid=scielo_issn,
+#             user=user,
+#             isis_created_date=classic_website_journal.isis_created_date,
+#             isis_updated_date=classic_website_journal.isis_updated_date,
+#             data=journal_data,
+#             status=MS_TO_MIGRATE,
+#             force_update=force_update,
+#         )
+#         migrated_journal.archive_incident_report()
+#     except Exception as e:
+#         exc_type, exc_value, exc_traceback = sys.exc_info()
+
+#         try:
+#             migrated_journal = MigratedJournal.get(
+#                 collection=collection,
+#                 pid=scielo_issn,
+#             )
+#             migrated_journal.add_incident(
+#                 collection_acron=collection.acron,
+#                 app_name="migration",
+#                 user=user,
+#                 context=context,
+#                 item_name="journal",
+#                 item_id=scielo_issn,
+#                 e=e,
+#                 message=None,
+#                 exc_traceback=exc_traceback,
+#             )
+#         except MigratedJournal.DoesNotExist:
+#             Incident.create(
+#                 collection_acron=collection.acron,
+#                 app_name="migration",
+#                 item_name="journal",
+#                 item_id=scielo_issn,
+#                 message=None,
+#                 context=context,
+#                 e=e,
+#                 exc_traceback=exc_traceback,
+#                 creator=user,
+#             )
 
 
 def create_or_update_migrated_journal_record(
@@ -76,39 +164,43 @@ def create_or_update_migrated_journal_record(
     journal_data,
     force_update=False,
 ):
-    """
-    Create/update MigratedJournal
-    """
     try:
-        # obtém classic website journal
+        migrated_journal = None
+
+        context = "get-data-from-classic-website"
         classic_website_journal = classic_ws.Journal(journal_data)
 
+        context = "migrate-data"
         migrated_journal = MigratedJournal.create_or_update(
             collection=collection,
             pid=scielo_issn,
-            creator=user,
+            user=user,
             isis_created_date=classic_website_journal.isis_created_date,
             isis_updated_date=classic_website_journal.isis_updated_date,
             data=journal_data,
             status=MS_TO_MIGRATE,
             force_update=force_update,
         )
-        return migrated_journal
     except Exception as e:
         exc_type, exc_value, exc_traceback = sys.exc_info()
 
-        message = _("Unable to migrate journal {} {}").format(
-            collection.acron, scielo_issn
-        )
-        MigrationFailure.create(
+        if not migrated_journal:
+            migrated_journal = MigratedJournal.create_or_update(
+                collection=collection,
+                pid=scielo_issn,
+                user=user,
+            )
+
+        migrated_journal.add_incident(
             collection_acron=collection.acron,
-            migrated_item_name="journal",
-            migrated_item_id=scielo_issn,
-            message=message,
-            action_name="migrate",
+            app_name="migration",
+            user=user,
+            context=context,
+            item_name="journal",
+            item_id=scielo_issn,
             e=e,
+            message=None,
             exc_traceback=exc_traceback,
-            creator=user,
         )
 
 
@@ -159,17 +251,17 @@ def create_or_update_journal(
         return migrated_journal
     except Exception as e:
         exc_type, exc_value, exc_traceback = sys.exc_info()
-
         message = _("Unable to create or update journal {}").format(migrated_journal)
-        MigrationFailure.create(
+        migrated_journal.add_incident(
             collection_acron=collection.acron,
-            migrated_item_name="journal",
-            migrated_item_id=migrated_journal.pid,
-            message=message,
-            action_name="create_or_update",
+            app_name="migration",
+            user=user,
+            context="create_or_update",
+            item_name="journal",
+            item_id=migrated_journal.pid,
             e=e,
+            message=message,
             exc_traceback=exc_traceback,
-            creator=user,
         )
 
 
@@ -181,7 +273,7 @@ def migrate_issue_db(
     """
     Migra os registros da base de dados issue
     """
-    classic_website = get_classic_website(collection.acron)
+    classic_website = get_classic_website(collection.acron, user)
 
     for issue_pid, issue_data in classic_website.get_issues_pids_and_records():
         migrated_issue = create_or_update_migrated_issue_record(
@@ -206,6 +298,8 @@ def create_or_update_migrated_issue_record(
     Create/update MigratedIssue
     """
     try:
+        migrated_issue = None
+
         classic_website_issue = classic_ws.Issue(issue_data)
 
         if classic_website_issue.is_press_release:
@@ -216,29 +310,35 @@ def create_or_update_migrated_issue_record(
         migrated_issue = MigratedIssue.create_or_update(
             collection=collection,
             pid=issue_pid,
-            creator=user,
+            user=user,
             isis_created_date=classic_website_issue.isis_created_date,
             isis_updated_date=classic_website_issue.isis_updated_date,
             status=status,
             data=issue_data,
             force_update=force_update,
         )
-        migrated_issue.migrated_journal = MigratedJournal.get(collection, scielo_issn)
-        migrated_issue.save()
         return migrated_issue
     except Exception as e:
         exc_type, exc_value, exc_traceback = sys.exc_info()
 
+        if not migrated_issue:
+            migrated_issue = MigratedIssue.create_or_update(
+                collection=collection,
+                pid=issue_pid,
+                user=user,
+            )
+
         message = _("Unable to migrate issue {} {}").format(collection.acron, issue_pid)
-        MigrationFailure.create(
+        migrated_issue.add_incident(
             collection_acron=collection.acron,
-            migrated_item_name="issue",
-            migrated_item_id=issue_pid,
+            app_name="migration",
+            user=user,
+            context="migrate",
+            item_name="issue",
+            item_id=issue_pid,
             message=message,
-            action_name="migrate",
             e=e,
             exc_traceback=exc_traceback,
-            creator=user,
         )
 
 
@@ -251,9 +351,8 @@ def create_or_update_issue(
     Create/update Issue e SciELOIssue
     """
     try:
-        scielo_journal = migrated_issue.migrated_journal.scielo_journal
-
         classic_website_issue = classic_ws.Issue(migrated_issue.data)
+        scielo_journal = SciELOJournal.get(scielo_issn=classic_website_issue.journal)
 
         issue = Issue.get_or_create(
             journal=scielo_journal.journal,
@@ -284,27 +383,27 @@ def create_or_update_issue(
         message = _("Unable to create or update issue {} {}").format(
             migrated_issue.collection.acron, migrated_issue.pid
         )
-        MigrationFailure.create(
+        migrated_issue.add_incident(
+            user=user,
             collection_acron=migrated_issue.collection.acron,
-            migrated_item_name="issue",
-            migrated_item_id=migrated_issue.pid,
-            message=message,
-            action_name="create_or_update",
+            app_name="migration",
+            context="create_or_update",
+            item_name="issue",
+            item_id=migrated_issue.pid,
             e=e,
+            message=message,
             exc_traceback=exc_traceback,
-            creator=user,
         )
 
 
 class IssueMigration:
     def __init__(self, user, collection_acron, migrated_issue, force_update):
-        self.classic_website = get_classic_website(collection_acron)
+        self.classic_website = get_classic_website(collection_acron, user)
         self.collection_acron = collection_acron
         self.force_update = force_update
         self.migrated_issue = migrated_issue
-        self.migrated_journal = migrated_issue.migrated_journal
         self.scielo_issue = self.migrated_issue.scielo_issue
-        self.scielo_journal = self.migrated_journal.scielo_journal
+        self.scielo_journal = self.scielo_issue.scielo_journal
         self.journal_acron = self.scielo_journal.acron
         self.user = user
 
@@ -358,7 +457,7 @@ class IssueMigration:
                     lang=file.get("lang"),
                     part=file.get("part"),
                     pkg_name=file.get("key"),
-                    creator=self.user,
+                    user=self.user,
                     force_update=self.force_update,
                 )
                 migrated += 1
@@ -369,15 +468,16 @@ class IssueMigration:
                 message = _("Unable to migrate issue files {} {}").format(
                     self.migrated_issue.collection.acron, file
                 )
-                MigrationFailure.create(
+                self.migrated_issue.add_incident(
                     collection_acron=self.migrated_issue.collection.acron,
-                    migrated_item_name="issue files",
-                    migrated_item_id=self.migrated_issue.pid,
+                    app_name="migration",
+                    user=self.user,
+                    context="migrate",
+                    item_name="issue files",
+                    item_id=self.migrated_issue.pid,
                     message=message,
-                    action_name="migrate",
                     e=e,
                     exc_traceback=exc_traceback,
-                    creator=self.user,
                 )
 
         self.migrated_issue.files_status = (
@@ -411,15 +511,16 @@ def import_one_issue_files(
         message = _("Unable to migrate issue {} {}").format(
             migrated_issue.collection.acron, migrated_issue.pid
         )
-        MigrationFailure.create(
+        migrated_issue.add_incident(
             collection_acron=migrated_issue.collection.acron,
-            migrated_item_name="files",
-            migrated_item_id=migrated_issue.pid,
+            app_name="migration",
+            user=user,
+            context="migrate",
+            item_name="files",
+            item_id=migrated_issue.pid,
             message=message,
-            action_name="migrate",
             e=e,
             exc_traceback=exc_traceback,
-            creator=user,
         )
 
 
@@ -434,6 +535,7 @@ def import_one_issue_document_records(
     Cria registros MigratedDocument com dados obtidos de base de dados ISIS
     de artigos
     """
+
     if migrated_issue.docs_status != MS_TO_MIGRATE:
         if not force_update:
             logging.warning(
@@ -443,7 +545,7 @@ def import_one_issue_document_records(
             return
 
     collection = migrated_issue.collection
-    classic_website = get_classic_website(collection.acron)
+    classic_website = get_classic_website(collection.acron, user)
 
     j = MigratedJournal.get(collection=collection, pid=migrated_issue.pid[:9])
     journal_issue_and_doc_data = {"title": j.data}
@@ -457,6 +559,9 @@ def import_one_issue_document_records(
         migrated_issue.pid,
     ):
         try:
+            migrated_document = None
+            document_pid = None
+
             logging.info(f"Records: {doc_id} {issue_folder}")
             if len(doc_records) == 1:
                 # é possível que em source_file_path exista registro tipo i
@@ -478,25 +583,26 @@ def import_one_issue_document_records(
                 force_update,
                 classic_website,
             )
-
-            if migrated_document:
-                pids.add(migrated_document.pid[1:-5])
+            document_pid = migrated_document.pid[1:-5]
+            if document_pid:
+                pids.add(document_pid)
         except Exception as e:
             exc_type, exc_value, exc_traceback = sys.exc_info()
-
-            pids_failed.add(migrated_document.pid[1:-5])
+            if document_pid:
+                pids.add(document_pid)
             message = _("Unable to migrate documents {} {} {} {}").format(
                 collection.acron, journal_acron, issue_folder, doc_id
             )
-            MigrationFailure.create(
+            migrated_issue.add_incident(
                 collection_acron=collection.acron,
-                migrated_item_name="document",
-                migrated_item_id=f"{journal_acron} {issue_folder} {doc_id}",
+                app_name="migration",
+                user=user,
+                item_name="document",
+                item_id=f"{journal_acron} {issue_folder} {doc_id}",
                 message=message,
-                action_name="migrate",
+                context="migrate",
                 e=e,
                 exc_traceback=exc_traceback,
-                creator=user,
             )
     # atualiza MigratedIssue.docs_status com MS_IMPORTED
     # for pid in pids:
@@ -519,6 +625,7 @@ def create_or_update_document_record(
     classic_website,
 ):
     try:
+        migrated_document = None
         collection = migrated_issue.collection
 
         logging.info(
@@ -568,7 +675,7 @@ def create_or_update_document_record(
             pid,
             migrated_issue,
             pkg_name=pkg_name,
-            creator=user,
+            user=user,
             isis_created_date=classic_ws_doc.isis_created_date,
             isis_updated_date=classic_ws_doc.isis_updated_date,
             data=journal_issue_and_doc_data,
@@ -580,19 +687,28 @@ def create_or_update_document_record(
             status = MS_IMPORTED
         migrated_document.status = status
         migrated_document.save()
-        logging.info(f"Saved {migrated_document}")
         return migrated_document
     except Exception as e:
         exc_type, exc_value, exc_traceback = sys.exc_info()
 
         message = _("Unable to migrate records {} {}").format(collection.acron, pid)
-        MigrationFailure.create(
+        if not migrated_document:
+            migrated_document = MigratedDocumentModel.create_or_update(
+                collection,
+                pid,
+                migrated_issue,
+                pkg_name=pkg_name,
+                user=user,
+            )
+
+        migrated_document.add_incident(
             collection_acron=collection.acron,
-            migrated_item_name="doc_records",
-            migrated_item_id=f"{pid}",
+            app_name="migration",
+            user=user,
+            context="migrate",
+            item_name="doc_records",
+            item_id=f"{pid}",
             message=message,
-            action_name="migrate",
             e=e,
             exc_traceback=exc_traceback,
-            creator=user,
         )
