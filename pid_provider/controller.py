09420bfb1d3b32134ad7b18fdb4e18ef9445cf1d
Roberta Takenaka <roberta.takenaka@scielo.org>
2024-01-13 15:15:01 -0300

    Refatora pid_provider.controller separando PidProvider em PidProvider e BasePidProvider

--- pid_provider/controller.py
+++ pid_provider/controller.py
@@ -12,13 +12,10 @@ LOGGER = logging.getLogger(__name__)
 LOGGER_FMT = "%(asctime)s [%(levelname)s] %(name)s: %(message)s"
 
 
-class PidProvider:
-    """
-    Recebe XML para validar ou atribuir o ID do tipo v3
-    """
+class BasePidProvider:
 
     def __init__(self):
-        self.pid_provider_api = PidProviderAPIClient()
+        pass
 
     def provide_pid_for_xml_zip(
         self,
@@ -50,7 +47,7 @@ class PidProvider:
         except Exception as e:
             exc_type, exc_value, exc_traceback = sys.exc_info()
             UnexpectedEvent.create(
-                e=e,
+                exception=e,
                 exc_traceback=exc_traceback,
                 detail={
                     "operation": "PidProvider.provide_pid_for_xml_zip",
@@ -90,7 +87,7 @@ class PidProvider:
         except Exception as e:
             exc_type, exc_value, exc_traceback = sys.exc_info()
             UnexpectedEvent.create(
-                e=e,
+                exception=e,
                 exc_traceback=exc_traceback,
                 detail={
                     "operation": "PidProvider.provide_pid_for_xml_uri",
@@ -118,51 +115,6 @@ class PidProvider:
                 is_published=is_published,
                 origin=xml_uri,
             )
-
-    def provide_pid_for_xml_with_pre(
-        self,
-        xml_with_pre,
-        name,
-        user,
-        origin_date=None,
-        force_update=None,
-        is_published=None,
-        origin=None,
-    ):
-        """
-        Recebe um xml_with_pre para solicitar o PID da versão 3
-        para o Pid Provider
-
-        Se o xml_with_pre já está registrado local e remotamente,
-        apenas retorna os dados registrados
-        {
-            'registered': {...},
-            'required_local_registration': False,
-            'required_remote_registration': False,
-        }
-
-        Caso contrário, solicita PID versão 3 para o Pid Provider e
-        armazena o resultado
-        """
-        response = self.pre_registration(xml_with_pre, name)
-        if response.get("required_local_registration"):
-            registered = PidProviderXML.register(
-                xml_with_pre,
-                name,
-                user,
-                origin_date=origin_date,
-                force_update=force_update,
-                is_published=is_published,
-                origin=origin,
-                synchronized=bool(response.get("xml_uri")),
-                error_type=response.get("error_type"),
-                error_msg=response.get("error_msg"),
-                traceback=response.get("traceback"),
-            )
-        else:
-            registered = response.get("registered")
-        logging.info(f"provide_pid_for_xml_with_pre result: {registered}")
-        return registered
 
     @classmethod
     def is_registered_xml_with_pre(cls, xml_with_pre, origin):
@@ -204,7 +156,7 @@ class PidProvider:
         except Exception as e:
             exc_type, exc_value, exc_traceback = sys.exc_info()
             UnexpectedEvent.create(
-                e=e,
+                exception=e,
                 exc_traceback=exc_traceback,
                 detail={
                     "operation": "PidProvider.is_registered_xml_uri",
@@ -241,7 +193,7 @@ class PidProvider:
         except Exception as e:
             exc_type, exc_value, exc_traceback = sys.exc_info()
             UnexpectedEvent.create(
-                e=e,
+                exception=e,
                 exc_traceback=exc_traceback,
                 detail={
                     "operation": "PidProvider.is_registered_xml_zip",
@@ -255,6 +207,109 @@ class PidProvider:
                 "error_type": str(type(e)),
             }
 
+    def provide_pid_for_xml_with_pre(
+        self,
+        xml_with_pre,
+        name,
+        user,
+        origin_date=None,
+        force_update=None,
+        is_published=None,
+        origin=None,
+    ):
+        """
+        Recebe um xml_with_pre para solicitar o PID da versão 3
+        para o Pid Provider
+
+        Se o xml_with_pre já está registrado local e remotamente,
+        apenas retorna os dados registrados
+        {
+            'registered': {...},
+            'required_local_registration': False,
+            'required_remote_registration': False,
+        }
+
+        Caso contrário, solicita PID versão 3 para o Pid Provider e
+        armazena o resultado
+        """
+        v3 = xml_with_pre.v3
+        response = self.pre_registration(xml_with_pre, name)
+        if response.get("required_local_registration"):
+            registered = PidProviderXML.register(
+                xml_with_pre,
+                name,
+                user,
+                origin_date=origin_date,
+                force_update=force_update,
+                is_published=is_published,
+                origin=origin,
+            )
+        else:
+            registered = response.get("registered")
+
+        # registered["xml_changed"] = v3 != xml_with_pre.v3
+        # registered["xml_uri"] = response.get("xml_uri")
+        logging.info(f"PidProvider.provide_pid_for_xml_with_pre: registered={registered}")
+
+        response.update(registered)
+        return response
+
+
+class PidProvider(BasePidProvider):
+    """
+    Recebe XML para validar ou atribuir o ID do tipo v3
+    """
+
+    def __init__(self):
+        self.pid_provider_api = PidProviderAPIClient()
+
+    def provide_pid_for_xml_with_pre(
+        self,
+        xml_with_pre,
+        name,
+        user,
+        origin_date=None,
+        force_update=None,
+        is_published=None,
+        origin=None,
+    ):
+        """
+        Recebe um xml_with_pre para solicitar o PID da versão 3
+        para o Pid Provider
+
+        Se o xml_with_pre já está registrado local e remotamente,
+        apenas retorna os dados registrados
+        {
+            'registered': {...},
+            'required_local_registration': False,
+            'required_remote_registration': False,
+        }
+
+        Caso contrário, solicita PID versão 3 para o Pid Provider e
+        armazena o resultado
+        """
+        v3 = xml_with_pre.v3
+        response = self.pre_registration(xml_with_pre, name)
+        if response.get("required_local_registration"):
+            registered = PidProviderXML.register(
+                xml_with_pre,
+                name,
+                user,
+                origin_date=origin_date,
+                force_update=force_update,
+                is_published=is_published,
+                origin=origin,
+            )
+        else:
+            registered = response.get("registered")
+
+        response.update(registered)
+
+        response["xml_changed"] = v3 != xml_with_pre.v3
+        logging.info(f"PidProvider.provide_pid_for_xml_with_pre: registered={response}")
+
+        return response
+
     def pre_registration(self, xml_with_pre, name):
         # verifica a necessidade de registro local e/ou remoto
 
@@ -265,30 +320,41 @@ class PidProvider:
             return demand
 
         response = {}
-        if demand["required_remote_registration"]:
+        if demand.pop("required_remote_registration"):
             response = self.pid_provider_api.provide_pid(xml_with_pre, name)
 
         response.update(demand)
+        logging.info(f"PidProvider.pre_registration: response: {response}")
         return response
 
-    def synchronize(self, user):
-        """
-        Identifica no pid provider local os registros que não
-        estão sincronizados com o pid provider remoto (central) e
-        faz a sincronização, registrando o XML local no pid provider remoto
-        """
-        if not self.pid_provider_api.pid_provider_api_post_xml:
-            raise ValueError(
-                _(
-                    "Unable to synchronized data with central pid provider because API URI is missing"
-                )
-            )
-        for item in PidProviderXML.unsynchronized():
-            name = item.pkg_name
-            xml_with_pre = item.xml_with_pre
-            response = self.pid_provider_api.provide_pid(xml_with_pre, name)
-            item.set_synchronized(user, **response)
-            # if response.get("synchronized"):
-            #     article_proc = ArticleProc.objects.get(sps_pkg__pid_v3=item.pid_v3)
-            #     article_proc.sps_pkg_status = tracker_choices.PROGRESS_STATUS_DONE
-            #     article_proc.save()
+    # def synchronize(self, user, ArticleProc):
+    #     """
+    #     Identifica no pid provider local os registros que não
+    #     estão sincronizados com o pid provider remoto (central) e
+    #     faz a sincronização, registrando o XML local no pid provider remoto
+    #     """
+    #     if not self.pid_provider_api.pid_provider_api_post_xml:
+    #         raise ValueError(
+    #             _(
+    #                 "Unable to synchronized data with central pid provider because API URI is missing"
+    #             )
+    #         )
+    #     for item in PidProviderXML.unsynchronized():
+    #         name = item.pkg_name
+    #         xml_with_pre = item.xml_with_pre
+
+    #         response = self.provide_pid_for_xml_with_pre(
+    #             xml_with_pre,
+    #             name,
+    #             user,
+    #             origin_date=None,
+    #             force_update=None,
+    #             is_published=None,
+    #             origin=None,
+    #         )
+    #         if response.get("synchronized") and ArticleProc:
+    #             try:
+    #                 article_proc = ArticleProc.objects.get(sps_pkg__pid_v3=item.pid_v3)
+    #                 article_proc.update_sps_pkg_status()
+    #             except ArticleProc.DoesNotExist:
+    #                 pass
