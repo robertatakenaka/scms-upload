96e37493475863e2dd91f79a646e9b8675742dbd
Roberta Takenaka <roberta.takenaka@scielo.org>
2024-07-21 22:45:57 -0300

    Altera a ordem das opções de menu para facilitar o acesso dos usuários

--- config/menu.py
+++ config/menu.py
@@ -1,17 +1,19 @@
 
 WAGTAIL_MENU_APPS_ORDER = [
+    None,
+    "upload",
+    "processing",
     "Tarefas",
     "unexpected-error",
-    "processing",
-    "migration",
-    "journal",
-    "issue",
     "article",
+    "issue",
+    "journal",
+    "collection",
+    "migration",
+    "pid_provider",
     "institution",
     "location",
     "researcher",
-    "collection",
-    "pid_provider",
     "Configurações",
     "Relatórios",
     "Images",
@@ -19,9 +21,9 @@ WAGTAIL_MENU_APPS_ORDER = [
     "Ajuda",
 ]
 
+
 def get_menu_order(app_name):
     try:
-        return WAGTAIL_MENU_APPS_ORDER.index(app_name) + 1
+        return WAGTAIL_MENU_APPS_ORDER.index(app_name)
     except:
         return 9000
-
